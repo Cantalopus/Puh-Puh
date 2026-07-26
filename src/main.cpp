579bf1cbcd67614c38220ee2ea2320f136c6c082
WR Allbritain <wes.allbritain@gmail.com>
2026-07-26 04:40:55 -0500

    add ball and paddles

--- src/main.cpp
+++ src/main.cpp
@@ -2,51 +2,41 @@
 
 int main()
 {
-    sf::RenderWindow window(
-        sf::VideoMode(800, 600),
-        "Moving Circle"
-    );
+    sf::RenderWindow window{
+        sf::VideoMode{1000, 600},
+        "pong"
+    };
 
     window.setFramerateLimit(60);
 
-    sf::CircleShape circle(50.f);
-    circle.setFillColor(sf::Color::Green);
-    circle.setPosition(350.f, 250.f);
+    sf::RectangleShape left_paddle{{20.0f, 120.0f}};
+    left_paddle.setPosition({50.0f, 240.0f});
 
-    constexpr float speed{7.f};
+    sf::RectangleShape right_paddle{{20.0f, 120.0f}};
+    right_paddle.setPosition({930.0f, 240.0f});
 
-    while (window.isOpen())
+    sf::CircleShape ball{12.0f};
+    ball.setOrigin({12.0f, 12.0f});
+    ball.setPosition({500.0f, 300.0f});
+
+    while(window.isOpen())
     {
-
         sf::Event event{};
 
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if(event.type == sf::Event::Closed)
             {
                 window.close();
             }
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            circle.move(-speed, 0.f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            circle.move(speed, 0.f);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            circle.move(0.f, -speed);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        {
-            circle.move(0.f, speed);
-        }
-        
-        window.clear(sf::Color(30,30,60));
-        window.draw(circle);
-        window.display();
+        window.clear();
+
+        window.draw(left_paddle);
+        window.draw(right_paddle);
+        window.draw(ball);
+
+        window.display(); 
     }
 }
